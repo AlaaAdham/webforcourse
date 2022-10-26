da06a93fca80f00d74ad60f83fbf76ba2b312e4d
Batoolkharraz <batoolkhelf@gmail.com>
2022-10-26 22:14:42 +0300

    commit

---
+++ js/edit.js
@@ -0,0 +1,14 @@
+function design(x){
+    document.getElementById(x).style.borderBottomColor ="#05324488";
+
+}
+
+function normal(x){
+    document.getElementById(x).style.borderBottomColor ="#0fa8e488";
+
+}
+
+function resetImg(){
+    document.getElementById("profile-img").src="img/blank-profile-picture-973460__340.webp";
+    document.getElementById("img-profile").src="img/blank-profile-picture-973460__340.webp";
+}
